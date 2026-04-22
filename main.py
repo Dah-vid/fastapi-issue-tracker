0920749215d81b26d6fdd95cd3e35fdf1d67d601
Dah-vid <davidtopair@gmail.com>
2026-04-22 08:25:03 +0100

    add issues router and storage functionality

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI #import
+from app.routes.issues import router as issues_router
 
-app = FastAPI() #initializing fast api
+app = FastAPI() 
+
+app.include_router(issues_router)
+
+#initializing fast api
 
