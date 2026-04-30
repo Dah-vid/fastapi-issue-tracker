8c5566a39da2a27214e67b4b70edaf25e45bf636
Dah-vid <davidtopair@gmail.com>
2026-04-30 06:42:44 +0100

    feat: add IssueStatus import to issues.py for enhanced issue management

--- app/routes/issues.py
+++ app/routes/issues.py
@@ -1,6 +1,6 @@
 import uuid # whats this
 from fastapi import APIRouter, HTTPException, status #whats this
-from app.schemas import IssueCreate, IssueOut, IssueUpdate
+from app.schemas import IssueCreate, IssueOut, IssueUpdate, IssueStatus
 from app.storage import load_data, save_data
 
 router = APIRouter(prefix="/api/v1/issues", tags= ["issues"])
